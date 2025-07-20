1d8ea07241a357812233659d04578f254cc61d90
Adrian Moring <adriantmoringbass@gmail.com>
2025-07-19 20:20:27 -0400

    Passes the npm tests

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,32 +1,33 @@
-const temperature = 90;
-const timeOfDay = "morning";
+let temperature = 90;
+let timeOfDay = "morning";
 let soilMoisture = 30;
 
 //If else statement turning water on or off
 
 if (temperature > 80){
 
-    console.log("Watering on")
+    console.log("Watering on");
 }
 else{
 
-    console.log("Watering off")
-}
+    console.log("Watering off");
+};
 
 //if else statement turning lights on or off
 
-if (timeOfDay === "evening" || "night"){
+if (timeOfDay === "evening" || timeOfDay === "night"){
 
-    console.log("Lights on")
+    console.log("Lights on");
 }
 else{
 
-    console.log("Lights off")
-}
+    console.log("Lights off");
+};
 
 //while loop raising soil moisture
 
-while (soilMoisture <= 40){
+while (soilMoisture < 40){
 
-    soilMoisture + 5
+    soilMoisture += 5;
 }
+console.log(soilMoisture)
